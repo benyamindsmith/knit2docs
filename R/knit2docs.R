8df7e3aa70178978c41f246bcc6077ae1ec06b4f
Benjamin Smith <46410142+benyamindsmith@users.noreply.github.com>
2023-04-23 23:31:19 -0400

    First Commit

---
+++ R/knit2docs.R
@@ -0,0 +1,22 @@
+#' Knit .Rmd File to Google Docs
+#'
+#' Knit a .Rmd file to a Google Doc (stored on Google Drive)
+#'
+#' @param rmd_file The file path of the .Rmd file that you want to knit.
+#' @param doc_name The title of the Doc
+#' @import rmarkdown
+#' @import googledrive
+#' @examples
+#' # NOTRUN
+#' knit2docs("Test.Rmd",doc_name=".Rmd to Docs")
+#'
+
+knit2docs<- function(rmd_file, doc_name=".Rmd to Docs"){
+  # Temporary File
+  temp_file<- tempfile(fileext = ".docx")
+  rmarkdown::render(rmd_file, "word_document", output_file = temp_file)
+  # Write to docs
+  googledrive::drive_upload(temp_file,
+                            name = doc_name,
+                            type = "application/vnd.google-apps.document")
+}
