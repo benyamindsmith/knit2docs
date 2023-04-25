8a2fe31278c965ceb6e7bb78aa96315a1a5b47ea
benyamindsmith <benyamindsmith@gmail.com>
2023-04-25 14:56:10 -0400

    overwrite and path args

--- R/knit2docs.R
+++ R/knit2docs.R
@@ -5,6 +5,7 @@
 #' @param rmd_file The file path of the .Rmd file that you want to knit.
 #' @param doc_name The title of the Doc
 #' @param overwrite Overwrite file if it exists. See the `overwrite` argument in googledrive::drive_upload()
+#' @param path specify the path where the Google Doc will live. See the `path` argument in googledrive::drive_upload()
 #' @import rmarkdown
 #' @import googledrive
 #' @examples
@@ -12,7 +13,10 @@
 #' knit2docs("Test.Rmd",doc_name=".Rmd to Docs")
 #'
 
-knit2docs<- function(rmd_file, doc_name=".Rmd to Docs", overwrite=TRUE){
+knit2docs<- function(rmd_file,
+                     doc_name=".Rmd to Docs",
+                     path = NULL,
+                     overwrite=TRUE){
   # Temporary File
   temp_file<- tempfile(fileext = ".docx")
   rmarkdown::render(rmd_file, "word_document", output_file = temp_file)
